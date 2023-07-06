7878ba8c1e029b42e1f5d1009fb526d57fb68ad3
Patrick Foessl <patrick.foessl@edu.fh-joanneum.at>
2023-07-06 10:06:53 +0200

    Delete javadoc comments from wrong branch

--- src/main/java/at/fhj/msd/Cocktail.java
+++ src/main/java/at/fhj/msd/Cocktail.java
@@ -1,34 +1,15 @@
-/**
 
- The at.fhj.msd package contains classes related to drinks and liquids.
- */
 package at.fhj.msd;
 import java.util.List;
 
-/**
 
- The Cocktail class represents a type of drink that can be created by combining multiple liquids.
-
- It extends the Drink class and provides additional functionality for managing cocktail ingredients.
- */
 public class Cocktail extends Drink {
     List<Liquid> ingredients;
 
-    /**
-
-     Creates a Cocktail object with the specified name and ingredients.
-     @param name the name of the cocktail
-     @param ingredients a list of Liquid objects representing the liquids in the cocktail
-     */
     public Cocktail(String name, List<Liquid> ingredients) {
         super(name);
         this.ingredients = ingredients;
     }
-    /**
-
-     Retrieves the volume of the cocktail.
-     @return the volume of the cocktail
-     */
     @Override
     public double getVolume() {
         double volume = 0;
@@ -37,11 +18,6 @@ public class Cocktail extends Drink {
         }
         return volume/ingredients.size();
     }
-    /**
-
-     Retrieves the alcohol percentage of the cocktail.
-     @return the alcohol percentage of the cocktail
-     */
     @Override
     public double getAlcoholPercent() {
         double alcohol = 0;
@@ -50,11 +26,6 @@ public class Cocktail extends Drink {
         }
         return alcohol/ingredients.size();
     }
-    /**
-
-     Checks if the cocktail is alcoholic.
-     @return true if the cocktail contains alcohol
-     */
     @Override
     public boolean isAlcoholic() {
         if (getAlcoholPercent() > 0){
@@ -62,19 +33,10 @@ public class Cocktail extends Drink {
         }
         else return false;
     }
-    /**
-
-     Retrieves the list of ingredients in the cocktail.
-     @return a list of Liquid objects representing the ingredients in the cocktail
-     */
     public List<Liquid> getIngredients() {
         return ingredients;
     }
-    /**
 
-     Sets the list of ingredients for the cocktail.
-     @param ingredients a list of Liquid objects representing the new ingredients for the cocktail
-     */
     public void setIngredients(List<Liquid> ingredients) {
         this.ingredients = ingredients;
     }
