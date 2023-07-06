9b5499dc7162d89916a17a83222649363338918a
Laurenz Altendorfer <laurenz.altendorfer@edu.fh-joanneum.at>
2023-07-06 11:08:47 +0200

    Create new DoubleQueue that a

---
+++ src/main/java/at/fhj/msd/DoubleQueue.java
@@ -0,0 +1,33 @@
+package at.fhj.msd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class DoubleQueue {
+    private List<Double> elements = new ArrayList<>();
+    private int maxSize;
+
+    public DoubleQueue(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public boolean offer(Double obj) {
+        if (elements.size() < maxSize) {
+            elements.add(obj);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Double poll() {
+        if (elements.size() > 0) {
+            Double element = elements.get(0);
+            elements.remove(0);
+            return element;
+        } else {
+            return null;
+        }
+    }
+}
