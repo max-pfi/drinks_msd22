3466c47a8a3ff085f6ef847d35eb2bbaa9be7282
max-pfi <m1999.pfisterer@gmail.com>
2023-07-06 12:15:05 +0200

    Fix bug in test for SimpleDrink

--- src/test/java/at/fhj/msd/SimpleDrinkTest.java
+++ src/test/java/at/fhj/msd/SimpleDrinkTest.java
@@ -8,37 +8,37 @@ public class SimpleDrinkTest {
     private Liquid water;
     private Liquid rum;
     private Liquid cola;
-    private SimpleDrink colaRum;
+    private SimpleDrink rumGlass;
     private SimpleDrink waterGlass;
 
     @BeforeEach
     public void setUp() {
         water = new Liquid("Water", 0.5, 0);
         rum = new Liquid("Rum", 0.2, 30);
-        cola = new Liquid("Cola", 0.3, 0);
 
-        colaRum = new SimpleDrink("ColaRum", cola);
+
+        rumGlass = new SimpleDrink("rumGlass", rum);
         waterGlass = new SimpleDrink("WaterGlass", water);
     }
 
     @Test
     public void testGetVolume() {
-        Assertions.assertEquals(waterGlass.getVolume(), 0.5);
+        Assertions.assertEquals(0.5, waterGlass.getVolume());
     }
 
     @Test
     public void testGetAlcoholPercentWithAlcohol() {
-        Assertions.assertEquals(colaRum.getAlcoholPercent(), 30);
+        Assertions.assertEquals(30, rumGlass.getAlcoholPercent());
     }
 
     @Test
     public void testGetAlcoholPercentWithoutAlcohol() {
-        Assertions.assertEquals(waterGlass.getAlcoholPercent(), 0);
+        Assertions.assertEquals(0, waterGlass.getAlcoholPercent());
     }
 
     @Test
     public void testIsAlcoholicWithAlcohol() {
-        Assertions.assertTrue(colaRum.isAlcoholic());
+        Assertions.assertTrue(rumGlass.isAlcoholic());
     }
 
     @Test
