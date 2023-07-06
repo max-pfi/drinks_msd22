deae989582fca8b65702bf31e87c87934539a85d
Patrick Foessl <patrick.foessl@edu.fh-joanneum.at>
2023-07-06 11:07:35 +0200

    Add javadoc for DrinkQueue

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -1,18 +1,37 @@
-package at.fhj.msd;
+/**
 
+ The DrinkQueue class represents a queue implementation specifically for drinks.
+ It stores Drink objects and provides methods for adding and removing drinks from the queue.
+ */
+package at.fhj.msd;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-public class DrinkQueue {
+/**
 
+ The DrinkQueue class represents a queue implementation specifically for drinks.
+
+ It stores Drink objects and provides methods for adding and removing drinks from the queue.
+ */
+public class DrinkQueue {
     private List<Drink> elements = new ArrayList<>();
     private int maxSize;
 
+    /**
+
+     Creates a DrinkQueue object with the specified maximum size.
+     @param maxSize the maximum size of the queue
+     */
     public DrinkQueue(int maxSize) {
         this.maxSize = maxSize;
     }
+    /**
 
+     Adds a drink to the queue.
+     @param obj the drink to be added to the queue
+     @return true if the drink was added successfully, false if the queue is full
+     */
     public boolean offer(Drink obj) {
         if (elements.size() < maxSize) {
             elements.add(obj);
@@ -21,15 +40,23 @@ public class DrinkQueue {
             return false;
         }
     }
+    /**
 
+     Retrieves and removes the first drink from the queue.
+     @return the first drink from the queue, or null if the queue is empty
+     */
     public Drink remove() {
         Drink element = poll();
         if (element == null) {
-            throw new NoSuchElementException("there's no element any more");
+            throw new NoSuchElementException("There is no element in the queue");
         }
         return element;
     }
+    /**
 
+     Retrieves and removes the first drink from the queue.
+     @return the first drink from the queue, or null if the queue is empty
+     */
     public Drink poll() {
         if (elements.size() > 0) {
             Drink element = elements.get(0);
@@ -39,7 +66,11 @@ public class DrinkQueue {
             return null;
         }
     }
+    /**
 
+     Retrieves, but does not remove, the first drink from the queue.
+     @return the first drink from the queue, or null if the queue is empty
+     */
     public Drink peek() {
         if (elements.size() > 0) {
             return elements.get(0);
@@ -47,13 +78,17 @@ public class DrinkQueue {
             return null;
         }
     }
+    /**
 
+     Retrieves, but does not remove, the first drink from the queue.
+     @return the first drink from the queue
+     @throws NoSuchElementException if the queue is empty
+     */
     public Drink element() {
         Drink element = peek();
         if (element == null) {
-            throw new NoSuchElementException("there's no element any more");
+            throw new NoSuchElementException("There is no element in the queue");
         }
         return element;
     }
-
 }
